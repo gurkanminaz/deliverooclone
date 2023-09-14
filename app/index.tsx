57050b16bf6893e50984251ef1da4aa5b3a37651
Gürkan Minaz <gurkanminaz@gmail.com>
2023-09-14 19:07:34 +0300

    horizontal scrolling

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import React from "react";
+import Categories from "@components/Categories";
+import Colors from "@constants/Colors";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Restaurants from "@components/Restaurants";
 
 const Page = () => {
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighbourhood</Text>
+        <Restaurants />
+        <Text style={styles.header}>Offers near you</Text>
+        <Restaurants />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    top: 50,
+    backgroundColor: Colors.lightGrey,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+});
 
 export default Page;
